dcc261eb5fdfb452b161b9c3dd800d74679b72eb
PiotrZPL <zpl20052020@protonmail.ch>
2023-03-25 11:02:52 +0100

    Added image precaching

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    precacheImage(const AssetImage("assets/images/icon_full_small.png"), context);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
